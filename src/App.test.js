54c7f0132aeb93134518686e6299d544adb4326e
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-09-04 12:23:36 -0700

    project complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getQueriesForElement, fireEvent } from "@testing-library/react";
 import App from "./App";
+import * as ReactDOM from 'react-dom';
+import { act } from "react-dom/test-utils";
 
-test("renders App without crashing", () => {
-  render(<App />);
+test("renders the correct content", () => {
+  const {getByText, getByLabelText} = render(<App />);
+
+  expect(getByText("Email*")).not.toBeNull();
 });
+
+// test("allows users to input fields", ()=> {
+//   const {getByText, getByLabelText} = render(<App />);
+
+//   const input = getByLabelText(/email/i);
+
+//   fireEvent.change(input, { target: {value:'ed@red.com'}})
+
+// })
+
+it('submits email', () => {
+  const { container } = render(<App />)
+  const name = container.querySelector('input[name="email"]')
+
+    fireEvent.change(name, {
+      target: {
+        value: "ed@red.com"
+      }
+    })
+    expect(name.value).toEqual("ed@red.com");
+})
